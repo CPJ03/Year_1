3734823ec1fba63de3ddf7b8296ac833c3e95cb3
Cheng Pin-Jie <chengpinjie03@gmail.com>
2023-09-26 17:39:54 +0800

    Add files via upload

---
+++ Lab 2/OOP_LAB_2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OOP_LAB2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Cheng Pin-Jie";
+            int age = 19;
+            int ID = 21000548;
+            Console.WriteLine("Hello my name is " +name+ ", my age is " +age+ ", with matric ID " +ID);
+        }
+    }
+}
