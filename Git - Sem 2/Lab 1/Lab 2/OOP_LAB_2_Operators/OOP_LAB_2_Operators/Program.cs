b3db83b742f96718dac48f110f4691e9f682d5ed
Cheng Pin-Jie <chengpinjie03@gmail.com>
2023-09-26 17:45:55 +0800

    Add files via upload

---
+++ Git - Sem 2/Lab 1/Lab 2/OOP_LAB_2_Operators/OOP_LAB_2_Operators/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OOP_LAB_2_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6;
+            int b = 2;
+
+            // Arithmetic Operators
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+            Console.WriteLine(a++);
+            Console.WriteLine(b--);
+
+            // Comparison Operators
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+            Console.WriteLine(a > b);
+            Console.WriteLine(a >= b);
+            Console.WriteLine(a < b);
+            Console.WriteLine(a <= b);
+        }
+    }
+}
