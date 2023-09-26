05e61292a458075b4f9cafbf228ebe8b88e955b7
Cheng Pin-Jie <chengpinjie03@gmail.com>
2023-09-26 17:50:05 +0800

    Add files via upload

---
+++ Git - Sem 2/Lab 1/Lab 7/OOP_Lab 7/OOP_Lab 7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace OOP_Lab_7
+{
+    abstract class animal
+    {
+        public abstract void sound();
+    }
+
+    class Cat : animal
+    {
+        public override void sound()
+        {
+            Console.WriteLine("Cat goes meow");
+        }
+    }
+
+    interface Ianimal
+    {
+        void DogSound();
+    }
+
+    class Dog: Ianimal
+    {
+        public void DogSound()
+        {
+            Console.WriteLine("Dog goes woff");
+        }
+    }
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Cat c1 = new Cat();
+            c1.sound();
+
+            Dog d1 = new Dog();
+            d1.DogSound();
+
+        }
+    }
+}
