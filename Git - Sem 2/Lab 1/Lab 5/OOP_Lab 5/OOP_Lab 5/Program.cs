2605463b6a735ecce21f306b7520eff98cb2ecf5
Cheng Pin-Jie <chengpinjie03@gmail.com>
2023-09-26 17:49:05 +0800

    Add files via upload

---
+++ Git - Sem 2/Lab 1/Lab 5/OOP_Lab 5/OOP_Lab 5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP_Lab_5
+{
+    class Student
+    {
+        public string name;
+        public int id;
+
+        public void rideToClass()
+        {
+            Console.WriteLine("I am going to ride to class.");
+        }
+
+        static void Main(string[] args)
+        {
+            // Creating object
+            Student stud = new Student();
+
+            // Field
+            stud.name = "Cheng Pin-Jie.";
+            stud.id = 21000548;
+
+            // Method and Display
+            Console.WriteLine("I am " + stud.name);
+            Console.WriteLine("My id is " + stud.id);
+            stud.rideToClass();
+
+        }
+    }
+}
---
+++ Git - Sem 2/Lab 1/Lab 6/OOP_Lab 6/OOP_Lab 6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OOP_Lab_6
+{
+    class Animal
+    {
+        private string type;
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create object
+            Animal cat1 = new Animal();
+            cat1.Type = "Cat";
+                Console.WriteLine(cat1.Type);
+        }
+    }
+}
